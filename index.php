ae66288137b4b5dabe2a1aae2b4396130ed5c2c4
Bassel212 <9026378@student.zadkine.nl>
2025-04-09 06:14:02 +0200

    Login-sys + Database

--- index.php
+++ index.php
@@ -1,24 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
-<header>
-    <nav>
-        <a href="/index.php">Home</a>
-        <a href="/register.php">Login</a>
-    </nav>
-    <main>
-        <h2>Registreren</h2>
-        <form action="register.php" method="POST">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="email" name="email" placeholder="E-mail" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit">Register</button>
-        </form>
-    </main>
-</body>
-</html>
+<?php include_once('./partials/header.php'); ?>
+
+<section class="hero">
+  <div class="hero-body">
+    <p class="title">
+      Home Page 
+    </p>
+    <p class="subtitle">
+      Welcome To our Website ...
+    </p>
+   
+  </div>
+</section>
+
+<?php include_once('./partials/footer.php'); ?>
